904a89d93c9c2f1992d0cd76c72d9baa1739ca50
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-10-28 12:53:07 +0900

    Fix

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -28,8 +30,8 @@ func main() {
 		if ti > len(cityFinds) {
 			log.Fatalf("ti is bigger than len(finds). ti is %v, len(finds) is %v\n", ti, len(cityFinds))
 		}
-		lat := ts.Find("TR").Eq(1).Find("TD").Eq(1).Text()
-		lon := ts.Find("TR").Eq(2).Find("TD").Eq(1).Text()
+		lat := dmsToDeg(ts.Find("TR").Eq(1).Find("TD").Eq(1).Text()[1:])
+		lon := dmsToDeg(ts.Find("TR").Eq(2).Find("TD").Eq(1).Text()[1:])
 		city := cityFinds[ti][1]
 
 		if city == "" {
@@ -40,4 +42,32 @@ func main() {
 		fmt.Println(lat)
 		fmt.Println(lon)
 	})
+}
+
+// 35°41′28.5576″ => 35.691266
+func dmsToDeg(dms string) float64 {
+	var degree float64
+	var min float64
+	var sec float64
+
+	degSplit := strings.Split(dms, "°")
+	if len(degSplit) != 2 {
+		log.Fatalf("invalid dms(degeee) format: %v\n", dms)
+	}
+	degree, _ = strconv.ParseFloat(degSplit[0], 64)
+
+	minSplit := strings.Split(degSplit[1], "′")
+	if len(minSplit) != 2 {
+		log.Fatalf("invalid dms(minute) format: %v\n", dms)
+	}
+	min, _ = strconv.ParseFloat(minSplit[0], 64)
+
+	l := len(minSplit[1]) - 3
+	if minSplit[1][l:] != "″" {
+		log.Fatalf("invalid dms(second) format: %v\n", dms)
+	}
+	// 30″ -> 30
+	sec, _ = strconv.ParseFloat(minSplit[1][:l], 64)
+
+	return degree + (min / 60) + (sec / 60 / 60)
 }
